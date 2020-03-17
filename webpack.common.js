ce20f693e4bc5b6b4d4cf92cb42e8e5466542c30
Jedrzej Klocek <jedrzejklocek@gmail.com>
2020-03-17 22:39:06 +0100

    Production build webpack config

--- webpack.common.js
+++ webpack.common.js
@@ -1,15 +1,30 @@
 const path = require('path');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
   entry: {
     app: './src/index.js'
   },
   plugins: [
-    new CleanWebpackPlugin(['dist']),
+    new Dotenv({
+      path: './.env.prod', // load this now instead of the ones in '.env'
+      safe: true, // load '.env.example' to verify the '.env' variables are all set. Can also be a string to a different file.
+      systemvars: true, // load all the predefined 'process.env' variables which will trump anything local per dotenv specs.
+      silent: true, // hide any errors
+      defaults: false // load '.env.defaults' as the default values if empty.
+    }),
+    new TerserPlugin({
+      parallel: true,
+      terserOptions: {
+        ecma: 6,
+      }
+    }),
+    new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      title: 'Production'
+      title: 'Mapy RadioPolska'
     })
   ],
   output: {
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,67 +1,66 @@
 const merge = require('webpack-merge');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 const common = require('./webpack.common.js');
+
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var path = require('path');
 var webpack = require('webpack');
 var autoprefixer = require('autoprefixer');
 
 module.exports = merge(common, {
-
-    module: {
-        loaders: [
-        {
-            test: /\.jsx?$/,
-            include: path.resolve(__dirname, 'src'),
-            loader: 'babel-loader',
-            exclude: /node_modules/,
-            query: {
-                cacheDirectory: true,
-                presets: [require.resolve('babel-preset-react-app')],
-            }
-        },
-        {
-            test: /\.css$/,
-            loader: ExtractTextPlugin.extract({ fallback: 'style-loader', use: [
-                    {
-                        loader: require.resolve('css-loader')
-                    },
-                    {
-                        loader: require.resolve('postcss-loader'),
-                        options: {
-                            plugins: () => [
-                                require('postcss-flexbugs-fixes'),
-                                autoprefixer({
-                                    browsers: [
-                                        '>1%',
-                                        'last 4 versions',
-                                        'Firefox ESR',
-                                        'not ie < 9',
-                                    ],
-                                    flexbox: 'no-2009',
-                                }),
-                            ],
-                        },
-                    },
-
-                ] })
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        include: path.resolve(__dirname, 'src'),
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query: {
+          cacheDirectory: true,
+          presets: [require.resolve('babel-preset-react-app')],
+        }
+      },
+      {
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract({
+          fallback: 'style-loader', use: [
+            {
+              loader: require.resolve('css-loader')
             },
             {
-                test: /\.json$/,
-                loader: 'json-loader'
+              loader: require.resolve('postcss-loader'),
+              options: {
+                plugins: () => [
+                  require('postcss-flexbugs-fixes'),
+                  autoprefixer({
+                    browsers: [
+                      '>1%',
+                      'last 4 versions',
+                      'Firefox ESR',
+                      'not ie < 9',
+                    ],
+                    flexbox: 'no-2009',
+                  }),
+                ],
+              },
             },
-            { test: /\.(png|jpeg|woff|woff2|eot|ttf|svg)$/, loader: 'url-loader?limit=100000' }
-        ]
-    },
-  
-    plugins: [
-        new UglifyJSPlugin({
-            parallel:  true,
-            sourceMap: true
-        }),
-        new webpack.DefinePlugin({
-            'process.env.NODE_ENV': JSON.stringify('production')
-        }),
-        new ExtractTextPlugin('static/css/[name].[hash].css')
+
+          ]
+        })
+      },
+      {
+        test: /\.json$/,
+        loader: 'json-loader'
+      },
+      { test: /\.(png|jpeg|woff|woff2|eot|ttf|svg)$/, loader: 'url-loader?limit=100000' }
     ]
+  },
+
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('production')
+    }),
+    new ExtractTextPlugin('static/css/[name].[hash].css')
+  ]
 });
